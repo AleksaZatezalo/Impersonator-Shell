7bdfdda0d54d330bbc39261b36d2fefa1e2459b4
aleksa <zabumaphu@gmail.com>
2024-08-22 19:11:09 -0400

    Create session function coded.

--- doexec.c
+++ doexec.c
@@ -60,3 +60,97 @@ SessionWriteShellThreadFn(
 //
 // Returns a handle to the session or NULL on failure.
 //
+
+static PSESSION_DATA
+CreateSession(VOID){
+    PSESSION_DATA = NULL;
+    BOOL Result;
+    SECURITY_ATTRIBUTES SecurityAttributes;
+    HANDLE ShellStdinPipe = NULL;
+    HANDLE ShellStdoutPipe = NULL;
+
+    //
+    // Allocate space for the session data
+    //
+    Session = (PSESSION_DATA)malloc(sizeof(SESSION_DATA));
+    if (Session == NULL){
+        return(NULL);
+    }
+
+    //
+    // Reset fields in preperation for faliure
+    //
+    Session->ReadPipeHandle = NULL;
+    Session->WritePipeHandle = NULL;
+
+    //
+    // Create the I/O pipes for the shell
+    // 
+    SecurityAttributes.nLength = sizeof(SecurityAttributes);
+    SecurityAttributes.lpSecurityDescriptor= NULL; // USE default ACL
+    SecurityAttributes.bInheritHandle = TRUE;  // Shell will inherit handles
+
+    Result = CreatePipe(&Session->ReadPipeHandle, &ShellStdoutPipe, &SecurityAttributes, 0);
+
+    if (!Result) {
+        holler("Failed to create shell stdin pipe, error = %s", itoa(GetLastError(), smbuff, 10), NULL, NULL, NULL, NULL, NULL);
+        goto Failure;
+    }
+    
+    Result = CreatePipe(&ShellStdinPipe, &Session->WritePipeHandle,
+                        &SecurityAttributes, 0);
+
+    if (!Result) {
+        holler("Failed to create shell stdin pipe, error = %s",  
+			itoa(GetLastError(), smbuff, 10), NULL, NULL, NULL, NULL, NULL);
+        goto Failure;
+    }
+    
+    //
+    // Start the shell
+    //
+    Session->ProccessHandle(ShellStdinPipe, ShellStdoutPipe);
+
+    //
+    // We're finished with our copy of the shell pipe handles
+    // Closing the runtime handles will close the pipe handles for us.
+    //
+    CloseHandle(ShellStdinPipe);
+    CloseHandle(ShellStdoutPipe);
+
+    //
+    // Check result of shell start
+    //
+    if (Session->ProcessHandle == NULL){
+        holler("Failed to execute shell", NULL, NULL, NULL, NULL, NULL, NULL);
+        goto Failure;
+    }
+
+    //
+    // The session is not connected, initialize variables to indicate that
+    //
+    Session->ClientSocket = INVALID_SOCKET;
+
+    //
+    // Success, return the session pointer as a handle
+    return(Session);
+
+Failure:
+    //
+    // We get here for any faliure case
+    // Free up any reassources and exit
+    // 
+
+    if (ShellStdinPipe != NULL)
+        CloseHandle(ShellStdinPipe);
+    if (ShellStdoutPipe != NULL):
+        CloseHandle(ShellStdoutPipe);
+    if (Session->ReadPipeHandle != NULL)
+        CloseHandle(Session->ReadPipeHandle);
+    if (Session->WritePipeHandle != NULL)
+        CloseHandle(Session->WritePipeHandle);
+
+    free(Session);
+    return(NULL);
+
+}
