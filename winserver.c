93ce400b1e684f64319ba084734f7cbe51f900b5
AleksaZatezalo <zabumaphu@gmail.com>
2024-09-11 23:35:29 -0500

    Winserver working

--- winserver.c
+++ winserver.c
@@ -1,14 +1,33 @@
-/*
-* A basic socket server made in windows.
-* To be removed later.
-*/
-
-
-#include <WinSock2.h>
+#include <winsock2.h>
 #include <Windows.h>
 #include <stdio.h>
 
-int main(int argc, char *argv[]){
-    printf("Simple Windows Server\n");
-    return 0;
+int main()
+{
+    WSADATA WSAData;
+    SOCKET server, client;
+    SOCKADDR_IN serverAddr, clientAddr;
+
+    WSAStartup(MAKEWORD(2,0), &WSAData);
+    server = socket(AF_INET, SOCK_STREAM, 0);
+
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(5555);
+
+    bind(server, (SOCKADDR *)&serverAddr, sizeof(serverAddr));
+    listen(server, 0);
+
+    printf("Listening for incoming connections...\n");
+
+    char buffer[1024];
+    int clientAddrSize = sizeof(clientAddr);
+    while((client = accept(server, (SOCKADDR *)&clientAddr, &clientAddrSize)) != INVALID_SOCKET)
+    {
+        printf("            Client connected!\n");
+        recv(client, buffer, sizeof(buffer), 0);
+        printf("            Client says: %s \n",buffer);
+        closesocket(client);
+        printf("Client disconnected.\n");
+    }
 }
