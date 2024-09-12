5bd64d854c837a18714c2b1b02b798aa3bd313ef
AleksaZatezalo <zabumaphu@gmail.com>
2024-09-11 22:55:05 -0500

    Creating basic server as example...

---
+++ winserver.c
@@ -0,0 +1,14 @@
+/*
+* A basic socket server made in windows.
+* To be removed later.
+*/
+
+
+#include <WinSock2.h>
+#include <Windows.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    printf("Simple Windows Server\n");
+    return 0;
+}
