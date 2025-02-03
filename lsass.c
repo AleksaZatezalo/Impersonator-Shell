070cc56263649911ccccc8bc6bf9a6c2e90ce8b7
aleksa <zabumaphu@gmail.com>
2025-02-02 23:33:02 -0500

    Including .h files

--- lsass.c
+++ lsass.c
@@ -3,3 +3,8 @@
 * Date: Februrary 2024
 * Description: LSASS is a C program that dumps password hashes from LSASSs. 
 */
+
+#include <stdio.h>
+#include <Windows.h>
+#include <DbgHelp.h>
+#pragma comment(lib, "DbgHelp.lib")
